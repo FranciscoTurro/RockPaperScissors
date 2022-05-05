e81b847f5ce1b23c87bbdbedf0d8ae1c4e2d8923
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-05-05 14:44:34 -0300

    Added small verifications

--- script.js
+++ script.js
@@ -1,57 +1,51 @@
 function computerPlay() {
-    let x = Math.floor(Math.random() * 3); 
+    let x = Math.floor(Math.random() * 3);
     if (x == 0) {
-        return('Rock');
-    }
-    else if (x == 1) {
-        return('Paper');
-    }
-    else {
-        return('Scissors');
+        return "Rock";
+    } else if (x == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
     }
 }
 
 function oneRound(playerChoice, computerChoice) {
-    if (playerChoice == 'Rock') {
-        if (computerChoice == 'Scissors') {
-            return('You won! Opponent chose scissors');
+    if (playerChoice == "Rock") {
+        if (computerChoice == "Scissors") {
+            return "You won! Opponent chose scissors";
+        } else if (computerChoice == "Paper") {
+            return "You lost! Opponent chose paper";
+        } else {
+            return "Its a draw! Opponent chose rock";
         }
-        else if (computerChoice == 'Paper') {
-            return('You lost! Opponent chose paper');
+    } else if (playerChoice == "Scissors") {
+        if (computerChoice == "Scissors") {
+            return "Its a draw! Opponen chose scissors";
+        } else if (computerChoice == "Paper") {
+            return "You won! Opponent chose paper";
+        } else {
+            return "You lost! Opponent chose rock";
         }
-        else {
-            return('Its a draw! Opponent chose rock');
+    } else if (playerChoice == "Paper") {
+        if (computerChoice == "Scissors") {
+            return "You lost! Opponent chose scissors";
+        } else if (computerChoice == "Paper") {
+            return "Its a draw! Opponent chose paper";
+        } else {
+            return "You won! Opponent chose rock";
         }
-    }
-
-    else if (playerChoice == 'Scissors') {
-        if (computerChoice == 'Scissors') {
-            return('Its a draw! Opponen chose scissors');
-        }
-        else if (computerChoice == 'Paper') {
-            return('You won! Opponent chose paper');
-        }
-        else {
-            return('You lost! Opponent chose rock');
-        }
-    }
-
-    else if (playerChoice == 'Paper') {
-        if (computerChoice == 'Scissors') {
-            return('You lost! Opponent chose scissors');
-        }
-        else if (computerChoice == 'Paper') {
-            return('Its a draw! Opponent chose paper');
-        }
-        else {
-            return('You won! Opponent chose rock');
-        }   
+    } else {
+        alert("Please only choose rock, paper or scissors!");
     }
 }
 function game() {
-    let x = prompt('How many games do you want to play?')
-    for (let i = 0;i < x;i++) {
-        alert((oneRound(prompt('what is your pick'),computerPlay())));
+    let x = prompt("How many games do you want to play?");
+    if (isNaN(x)) {
+        alert("Please choose only a number of games to play");
+    } else {
+        for (let i = 0; i < x; i++) {
+            alert(oneRound(prompt("what is your pick"), computerPlay()));
+        }
     }
 }
 game();
