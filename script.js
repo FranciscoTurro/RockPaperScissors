969bbc991722ff5e5d6e054a0d8bee02888d729b
FranciscoTurro <franciscoftrro@outlook.com.ar>
2023-07-16 19:58:10 -0300

    testing github remote

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ let playerCounter = 0;
 let machineCounter = 0;
 
 function computerPlay() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ['Rock', 'Paper', 'Scissors'];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function oneRound(playerChoice, computerChoice) {
-  if (playerChoice == "Rock") {
-    if (computerChoice == "Scissors") {
+  if (playerChoice == 'Rock') {
+    if (computerChoice == 'Scissors') {
       ++playerCounter;
-    } else if (computerChoice == "Paper") {
+    } else if (computerChoice == 'Paper') {
       ++machineCounter;
     } else {
       ++machineCounter;
       ++playerCounter;
     }
-  } else if (playerChoice == "Scissors") {
-    if (computerChoice == "Scissors") {
+  } else if (playerChoice == 'Scissors') {
+    if (computerChoice == 'Scissors') {
       ++playerCounter;
       ++machineCounter;
-    } else if (computerChoice == "Paper") {
+    } else if (computerChoice == 'Paper') {
       ++playerCounter;
     } else {
       ++machineCounter;
     }
-  } else if (playerChoice == "Paper") {
-    if (computerChoice == "Scissors") {
+  } else if (playerChoice == 'Paper') {
+    if (computerChoice == 'Scissors') {
       ++machineCounter;
-    } else if (computerChoice == "Paper") {
+    } else if (computerChoice == 'Paper') {
       ++machineCounter;
       ++playerCounter;
     } else {
@@ -36,27 +36,27 @@ function oneRound(playerChoice, computerChoice) {
     }
   }
 }
-const buttons = document.querySelectorAll("button"); //returns an array-like object called a node list
+const buttons = document.querySelectorAll('button'); //returns an array-like object called a node list
 buttons.forEach((buttons) => {
-  buttons.addEventListener("click", () => {
-    const img = buttons.querySelector("img");
+  buttons.addEventListener('click', () => {
+    const img = buttons.querySelector('img');
     oneRound(img.id, computerPlay());
-    const pcounter = document.querySelector("#playerCounter");
+    const pcounter = document.querySelector('#playerCounter');
     pcounter.textContent = playerCounter;
 
-    const ccounter = document.querySelector("#pcCounter");
+    const ccounter = document.querySelector('#pcCounter');
     ccounter.textContent = machineCounter;
     gameCheck();
   });
 });
 
 function gameCheck() {
-    if (playerCounter == 5) {
-        alert('w');
-        document.location.reload();
-    }
-    if (machineCounter == 5) {
-        alert('l');
-        document.location.reload();
-    }
+  if (playerCounter == 5) {
+    alert('w');
+    document.location.reload();
+  }
+  if (machineCounter == 5) {
+    alert('l');
+    document.location.reload();
+  }
 }
